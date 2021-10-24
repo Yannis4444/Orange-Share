d01c6025a200c12f8ad385ca67a0de8ae9672e6f
Yannis4444 <Di1tPfGH>
2021-10-24 03:49:38 +0200

    added windows installer

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,1 @@
-pyinstaller --onefile --collect-all "orangeshare" --add-data "orangeshare/logo/gray.png;orangeshare/logo" --name=orangeshare --icon=orangeshare/logo/white.ico main.py
-pyinstaller --onefile --collect-all "orangeshare" --add-data "orangeshare/logo/gray.png;orangeshare/logo" --noconsole --name=orangeshare-noconsole --icon=orangeshare/logo/white.ico main.py
-pyinstaller --onefile --collect-all "orangeshare" --add-data "orangeshare/logo/gray.png;orangeshare/logo" --noconsole --name=orangeshare-tray-icon --icon=orangeshare/logo/white.ico tray_icon.py
+pyinstaller --onefile --collect-all "orangeshare" --add-data "orangeshare/logo/gray.png;orangeshare/logo" --noconsole --name=orangeshare --icon=orangeshare/logo/white.ico main.py
