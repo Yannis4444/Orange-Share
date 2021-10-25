ed4e91259da8f3ee314597470f7fe90e7b7f2544
Yannis4444 <yannis44@web.de>
2021-10-25 21:57:35 +0200

    some clean up

--- orangeshare/__init__.py
+++ orangeshare/__init__.py
@@ -4,7 +4,6 @@ Orange-Share
 A small python server that accepts requests from an apple shortcut to allow sharing all sorts of media from iOS with any desktop OS
 """
 
-# TODO: set to correct version (1.6.1)
 __version__ = "1.7.0"
 __author__ = 'Yannis Vierkoetter'
 
