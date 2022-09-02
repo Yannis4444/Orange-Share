dd97e5b87307a2fcf7bba4f739399c3369460c5e
Yannis4444 <Di1tPfGH>
2022-09-02 20:41:32 +0200

    added console version for exe

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,4 @@
 pip install .
 pyinstaller --onefile --collect-all "orangeshare" --add-data "orangeshare/logo/gray.png;orangeshare/logo" --noconsole --name=orangeshare --icon=orangeshare/logo/white.ico main.py
+pyinstaller --onefile --collect-all "orangeshare" --add-data "orangeshare/logo/gray.png;orangeshare/logo" --name=orangeshare-console --icon=orangeshare/logo/white.ico main.py
 "D:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc windows_installer.iss
